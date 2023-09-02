a16b73ffa18ba2f528a8ba7fee8ea674b93839e4
이현재 <71202076+2-NOW@users.noreply.github.com>
2023-09-02 14:55:15 +0900

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    extend: {
+      screens: {
+        xs: "480px",
+        "3xl": "1920px",
+      },
+    },
+  },
+  plugins: [],
+};
