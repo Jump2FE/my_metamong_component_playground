50982a74f7c4c237c7796d1660f1ed2ac013ff4e
Santos <wjdrms1919@gmail.com>
2023-09-24 18:13:01 +0900

    [FEAT] 모달 적용

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from 'react-router-dom';
 import Chip from '../pages/chip';
 import Button from '../pages/button';
+import Modal from '../pages/modal';
 
 export default function AppRoutes() {
   return (
     <Routes>
       <Route path="/chip" element={<Chip />} />
       <Route path="/button" element={<Button />} />
+      <Route path="/modal" element={<Modal />} />
     </Routes>
   );
 }
