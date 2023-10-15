68b2b48ca882f2ad3a8d84f8e4e2633859c6ae22
Santos <wjdrms1919@gmail.com>
2023-10-15 11:39:17 +0900

    [WIP] CHIP

---
+++ src/components/Chip/Chip.style.ts
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const SideWrapper = styled.aside`
+  width: 16.25rem;
+  height: 100%;
+  position: fixed;
+  display: flex;
+  padding-top: 3rem;
+  padding-bottom: 3rem;
+  padding-left: 3rem;
+`;
+
+export const MainWrapper = styled.main`
+  padding-left: 2rem;
+  margin-left: 16.25rem;
+`;
---
+++ src/components/Chip/Chip.tsx
@@ -0,0 +1,3 @@
+export default function Chip() {
+  return <div>chip</div>;
+}
