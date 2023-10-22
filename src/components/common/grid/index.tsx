a1e66900c35471eacc30a7dba5c7caf070131a5a
2-NOW <kj109888@gmail.com>
2023-10-22 15:58:11 +0900

    style: refactor grid component to use CSS grid layout

--- src/components/common/grid/index.tsx
+++ src/components/common/grid/index.tsx
@@ -1,7 +1,41 @@
-export const Grid = () => {
+import type { CSSProperties, ReactNode } from "react";
+
+type GridItem = {
+  key: string;
+  minWidth: number;
+  render: ReactNode;
+};
+
+interface GridProps<Item extends GridItem> {
+  data: Item[];
+  columns: number;
+  gap?: `${number}px`;
+  align?: "left" | "center" | "right";
+  justify?: "left" | "center" | "right";
+}
+
+export const Grid = ({
+  data,
+  columns,
+  gap,
+  align,
+  justify,
+}: GridProps<GridItem>) => {
+  const style = {
+    display: "grid",
+    gridTemplateColumns: `repeat(${columns}, 1fr)`,
+    gap,
+    alignItems: align,
+    justifyItems: justify,
+  } satisfies CSSProperties;
+
   return (
-    <div>
-      <h1>Grid</h1>
+    <div style={style}>
+      {data.map(({ key, minWidth, render }) => (
+        <div key={key} style={{ minWidth }}>
+          {render}
+        </div>
+      ))}
     </div>
   );
 };
